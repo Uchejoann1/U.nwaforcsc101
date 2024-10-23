c049c853e6a8d7698e48ce5a4b081dcbd9e982ff
unknown <uchechukwu.nwafor@pau.edu.ng>
2024-10-23 13:37:51 -0700

    (Second commit!)

--- week-2/practice_2.rs
+++ week-2/practice_2.rs
@@ -1,8 +1,12 @@
 fn main() {
 	println!(); // print just a
+
 	println!("Hello ");//prints
+
 	println!("format {} arguments", "some"); //prints format some arguments
+
 	println!("My name is {}. I am the class rep of stream 1", "Dagogo Williams-Jumbo");
+	
 }
 
 
