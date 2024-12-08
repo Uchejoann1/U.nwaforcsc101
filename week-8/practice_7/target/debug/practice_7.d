be3b840d888b6e320e0828f14bffbb7f5d49d751
unknown <uchechukwu.nwafor@pau.edu.ng>
2024-12-08 14:36:11 -0800

    hello

--- week-8/practice_7/target/debug/practice_7.d
+++ week-8/practice_7/target/debug/practice_7.d
@@ -1,1 +1,1 @@
-C:\Users\PC\Documents\U.nwaforcsc101\week-7\practice_7\target\debug\practice_7.exe: C:\Users\PC\Documents\U.nwaforcsc101\week-7\practice_7\src\main.rs
+C:\Users\PC\Documents\U.nwaforcsc101\week-8\practice_7\target\debug\practice_7.exe: C:\Users\PC\Documents\U.nwaforcsc101\week-8\practice_7\src\main.rs
